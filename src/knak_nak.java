96e5e80c075f00f256c9e1e8c2738b41e9bc1f68
TheSimpleMango <mingda.he@gmail.com>
2016-08-01 18:30:07 -0700

    1
    1

---
+++ src/knak_nak.java
@@ -0,0 +1,12 @@
+import java.nio.channels.ShutdownChannelGroupException;
+
+import javax.swing.JOptionPane;
+
+public class knak_nak {
+public static void main(String[] args) {
+	JOptionPane.showInputDialog("knock knock");
+	JOptionPane.showInputDialog("europe");
+	JOptionPane.showMessageDialog(null, "no your a poo");
+	JOptionPane.showMessageDialog(null, "bye");
+}
+}
---
+++ src/one.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class one {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "eat pie");
+	JOptionPane.showMessageDialog(null, "eat pankackes");
+	JOptionPane.showMessageDialog(null, "eat pizza");
+	JOptionPane.showMessageDialog(null, "drink water");
+	JOptionPane.showMessageDialog(null, "eat steak");
+}
+}
